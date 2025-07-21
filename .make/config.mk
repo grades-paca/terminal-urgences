408538291f649e0d3a57b75f5dd089e882b64e2d
anthonys <asuccu@iess.fr>
2025-07-21 17:20:44 +0200

    Update for add vite + SF
    Concerne #000000

--- .make/config.mk
+++ .make/config.mk
@@ -5,6 +5,6 @@ PHP         = $(PHP_CONT) php
 COMPOSER    = $(PHP_CONT) composer
 SYMFONY     = $(PHP) bin/console
 
-TS_CONT     = $(DOCKER_COMP) exec typescript
+TS_CONT     = $(DOCKER_COMP) exec php
 TS_NPX      = $(TS_CONT) npx
 TS_NPM      = $(TS_CONT) npm
