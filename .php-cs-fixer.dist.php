e5d7d32c73b17b7626b76dfc434c1583a4b0b4bf
anthonys <asuccu@iess.fr>
2025-07-22 10:47:36 +0200

    Fix review (PhpCsFixer)
    Concerne #371044

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,8 +2,7 @@
 
 $finder = (new PhpCsFixer\Finder())
     ->in(__DIR__)
-    ->exclude('var')
-;
+    ->exclude(['var', 'vendor', 'typescript/node_modules', 'tests/Support/_generated/']);
 
 return (new PhpCsFixer\Config())
     ->setRules([
